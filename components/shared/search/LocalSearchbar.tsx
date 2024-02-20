eb19d6eed8046f4c7668ade236a6a62a92bd1782
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2024-02-20 17:46:16 +0530

    Heading, button, Localsearch, and filter components added

---
+++ components/shared/search/LocalSearchbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
+
+interface CustomInputProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses: string;
+}
+
+const LocalSearchbar = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: CustomInputProps) => {
+  return (
+    <div className="relative w-full ">
+      <div
+        className={`background-light800_darkgradient relative flex min-h-[56px] grow ${otherClasses} items-center gap-4 rounded-[10px] px-4`}
+      >
+        {/* We only wanna show this when Icon Position is on the left side */}
+        {iconPosition === "left" && (
+          <Image
+            src={imgSrc}
+            alt="search"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+
+        <Input
+          type="text"
+          placeholder={placeholder}
+          value=""
+          onChange={() => {}}
+          className="paragraph-regular placeholder background-light800_darkgradient no-focus border-none shadow-none outline-none "
+        />
+
+        {/* When we need icon on the right side */}
+        {iconPosition === "right" && (
+          <Image
+            src={imgSrc}
+            alt="search"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LocalSearchbar;
