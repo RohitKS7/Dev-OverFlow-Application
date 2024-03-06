551a820c67c52169a399e1f54662fe0f4c1b5933
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2024-03-06 09:40:45 +0530

    yo

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook",
+    "/api/webhook(.*)",
     "question/:id",
     "tags/:id",
     "tags",
@@ -14,7 +14,7 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhook(.*)", "/api/chatgpt"],
 });
 
 export const config = {
