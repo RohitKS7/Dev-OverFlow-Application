72d3609116515c3fcff2d1665ac2eda33914fda0
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2024-06-30 17:46:52 +0530

    feat: added hover and color change

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ interface ProjectLinks {
   projectlink: string;
   codelink: string;
   label: string;
+  type: string;
 }
 
 interface SocialLinks {
@@ -22,6 +23,7 @@ const projectLinks: ProjectLinks[] = [
     codelink: "https://github.com/RohitKS7/Admin_Dashboard",
 
     label: "ShopIt",
+    type: "(Web2)",
   },
   {
     imgURL: "/assets/images/metaversus.png",
@@ -29,34 +31,37 @@ const projectLinks: ProjectLinks[] = [
     codelink: "https://bootstraphub.com/RohitKS7/Metaverse-Modern-NEXT-13-Site",
 
     label: "Metaversus",
+    type: "(Web2)",
   },
   {
     imgURL: "/assets/images/nft-game.png",
     projectlink: "https://rohit-nft-card-game.vercel.app/",
     codelink: "https://github.com/RohitKS7/NFT_card_game",
-    label: "NFT Card Game (Web3)",
+    label: "NFT Card Game",
+    type: "(Web3)",
   },
   {
     imgURL: "/assets/images/crowdfunding.png",
     projectlink: "https://crowdfunding-by-rohit.netlify.app/",
     codelink: "https://github.com/RohitKS7/Crowd-Funding-Web3-App",
-    label: "CrowdFunding (Web3)",
+    label: "CrowdFunding",
+    type: "(Web3)",
   },
 ];
 
 const socialLinks: SocialLinks[] = [
   {
-    imgURL: "/assets/icons/github.svg",
+    imgURL: "/assets/icons/github-gray.svg",
     link: "https://twitter.com/SumanRohitK7",
     label: "Github",
   },
   {
-    imgURL: "/assets/icons/linkedin.svg",
+    imgURL: "/assets/icons/linkedIn-gray.svg",
     link: "https://linkedin.com/in/rohit-kumar-suman",
     label: "LinkedIn",
   },
   {
-    imgURL: "/assets/icons/twitterX.svg",
+    imgURL: "/assets/icons/twitterX-gray.svg",
     link: "https://github.com/RohitKS7",
     label: "twitterX",
   },
@@ -65,9 +70,9 @@ const IntroPage = () => {
   return (
     <main className="flex-center min-h-screen w-full bg-[#ffdada] ">
       {/* Floating Container */}
-      <div className="intro-shadow relative  flex h-[75dvh] w-full max-w-[75vw] flex-col items-center justify-around gap-8 bg-white">
+      <div className="intro-shadow relative  flex w-full max-w-[70vw] flex-col items-center justify-between gap-8 overflow-hidden  bg-[#f8f8ff]">
         {/* Creator & Project Name */}
-        <div className="flex-center flex-col gap-6">
+        <div className="flex-center mt-4 flex-col gap-6">
           <Link href="/home" className="mt-4 flex items-center gap-3">
             <Image
               src="/assets/images/site-logo.svg"
@@ -80,7 +85,7 @@ const IntroPage = () => {
               Dev <span className="text-primary-500">Overflow</span>
             </p>
           </Link>
-          <p className="paragraph-medium max-w-[75%] text-gray-700">
+          <p className="body-regular max-w-[75%] text-gray-700">
             DevOverflow is a complex Next.js 14 app that exhibits Q&A platform
             for developers to ask questions, share knowledge, and learn from
             each other.
@@ -92,18 +97,61 @@ const IntroPage = () => {
             </span>
           </p>
           {/* ⁡⁣⁢⁣Link to HomePage⁡ */}
-          <div className="paragraph-medium text-gray-700">
+          <div className="body-regular text-gray-700">
             Explore the
             <Link
               href="/home"
-              className="h2-semibold mx-2 cursor-pointer capitalize text-primary-500 underline hover:text-gray-700"
+              className="h3-semibold mx-2 cursor-pointer capitalize text-primary-500 underline hover:text-gray-700"
             >
               Application
             </Link>
           </div>
         </div>
+        {/* ⁡⁣⁢⁣More Projects Link⁡ */}
+        <div className="flex-center flex-col gap-6">
+          <div className="h3-semibold text-gray-500">More Projects By Me :</div>
+
+          <div className="flex flex-1 items-end justify-center gap-8">
+            {projectLinks.map((item) => (
+              <div
+                key={item.label + item.projectlink}
+                className="flex cursor-pointer flex-col items-center justify-between gap-8  bg-[#ffdada] px-3 pt-3"
+              >
+                <div className="flex-center max-w-40 flex-col gap-3">
+                  <Link
+                    href={item.codelink}
+                    className="body-semibold group relative w-full text-center text-gray-700"
+                  >
+                    <div>
+                      {item.label}
+                      <p className="mt-[2px]">{item.type}</p>
+                      <div className="paragraph-semibold project-hover-shadow absolute inset-x-0 top-[-40px] flex items-end justify-center bg-[#ffdada] p-[5px] text-gray-700 opacity-0 duration-300 group-hover:opacity-100">
+                        Code
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href={item.projectlink} className="group relative">
+                    <Image
+                      src={item.imgURL}
+                      alt={item.label}
+                      width={150}
+                      height={150}
+                      className="object-contain"
+                    />
+                    <div
+                      className="paragraph-semibold absolute inset-x-0 bottom-0 flex items-end justify-center bg-[#ffdada]
+text-gray-700 opacity-0 duration-300 group-hover:opacity-100"
+                    >
+                      Website
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         {/* ⁡⁣⁢⁣Social Links⁡ */}
-        <div className="absolute right-0">
+        <div className="absolute right-0 top-[35%]">
           {socialLinks.map((item) => (
             <Link
               key={item.imgURL}
@@ -113,43 +161,12 @@ const IntroPage = () => {
               <Image
                 src={item.imgURL}
                 alt={item.label}
-                width={50}
-                height={50}
-                className="object-contain"
+                width={30}
+                height={30}
+                className="my-1 object-contain"
               />
             </Link>
           ))}
-        </div>
-        {/* ⁡⁣⁢⁣More Projects Link⁡ */}
-        <div className="flex-center flex-col gap-6">
-          <div className="h3-semibold text-gray-500">More Projects By Me :</div>
-
-          <div className="flex flex-1 items-start justify-center gap-8">
-            {projectLinks.map((item) => (
-              <div
-                key={item.label + item.projectlink}
-                className="flex cursor-pointer flex-col items-center justify-between gap-8"
-              >
-                <div className="flex-center max-w-40 flex-col gap-3">
-                  <Link href={item.projectlink}>
-                    <Image
-                      src={item.imgURL}
-                      alt={item.label}
-                      width={150}
-                      height={150}
-                      className="object-contain"
-                    />
-                  </Link>
-                  <Link
-                    href={item.codelink}
-                    className="paragraph-medium mx-4 text-gray-700"
-                  >
-                    {item.label}
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </main>
